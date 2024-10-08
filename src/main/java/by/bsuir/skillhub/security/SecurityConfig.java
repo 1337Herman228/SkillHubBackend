c4788f45b6055a21d519acafc4a2b25d5080a429
EkaterinaKruk <ekaterunakruk2004@gmail.com>
2024-10-08 15:11:30 +0300

    swap hasAuthority in SecurityConfog to @PreAuthorize

--- src/main/java/by/bsuir/skillhub/security/SecurityConfig.java
+++ src/main/java/by/bsuir/skillhub/security/SecurityConfig.java
@@ -37,9 +37,12 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests( auth -> auth
-                        .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()
-                        .anyRequest().authenticated())
+//                .authorizeHttpRequests( auth -> auth
+//                        .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()
+//                        .requestMatchers(new AntPathRequestMatcher("/user/**")).hasAnyAuthority("user","admin","teacher")
+//                        .requestMatchers(new AntPathRequestMatcher("/teacher/**")).hasAnyAuthority("admin","teacher")
+//                        .requestMatchers(new AntPathRequestMatcher("/admin/**")).hasAnyAuthority("admin")
+//                        .anyRequest().authenticated())
                 .sessionManagement(configurer -> configurer
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
