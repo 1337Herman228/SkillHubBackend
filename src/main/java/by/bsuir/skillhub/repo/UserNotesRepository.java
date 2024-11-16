8e036dc43bda826dbf954429913badb001d15c12
EkaterinaKruk <ekaterunakruk2004@gmail.com>
2024-11-16 13:59:25 +0300

    feat: add notes logic

--- src/main/java/by/bsuir/skillhub/repo/UserNotesRepository.java
+++ src/main/java/by/bsuir/skillhub/repo/UserNotesRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserNotesRepository extends JpaRepository<UserNotes, Long> {
     List<UserNotes> findByLesson(Lessons lesson);
     List<UserNotes> findByUser(Users user);
+    Optional<UserNotes> findByLessonAndUser(Lessons lesson, Users user);
 }
