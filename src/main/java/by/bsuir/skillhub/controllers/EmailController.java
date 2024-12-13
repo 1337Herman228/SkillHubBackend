0e21ef113b505ae74274d1bd0aacc69c7d8715f6
EkaterinaKruk <ekaterunakruk2004@gmail.com>
2024-12-14 02:03:22 +0300

    feat: add email service

---
+++ src/main/java/by/bsuir/skillhub/controllers/EmailController.java
@@ -0,0 +1,24 @@
+package by.bsuir.skillhub.controllers;
+
+import by.bsuir.skillhub.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @GetMapping("/send-email")
+    public String sendEmail(
+            @RequestParam String to,
+            @RequestParam String subject,
+            @RequestParam String text) {
+        emailService.sendEmail(to, subject, text);
+        return "Email sent successfully!";
+    }
+}
+
